cb88e3498c66f4c6e307a38cd242712da36f1ebd
Newton Bofulo <newtonombese1@gmail.com>
2024-04-30 10:42:47 +0300

    Learning schema in an advanced level

--- src/drizzle/schema.ts
+++ src/drizzle/schema.ts
@@ -1,7 +1,19 @@
-import { pgTable, uuid, varchar } from 'drizzle-orm/pg-core'
+// import { table } from 'console'
+import {  } from 'drizzle-orm/mysql-core'
+import { integer, pgEnum, pgTable, uuid, varchar, uniqueIndex } from 'drizzle-orm/pg-core'
+
+export const UserRole = pgEnum("userRole", ["admin", "user"])
 
 export const UserTable = pgTable("user", {
   id: uuid("id").primaryKey().defaultRandom(),
   name: varchar("name", { length: 255 }).notNull(),
+  age: integer("age").notNull(),
+  email: varchar("email", { length: 255 }).notNull(),
+  role: UserRole("userRole").default("user").notNull()
+}, table => {
+  return {
+    emailIndex: uniqueIndex("email").on(table.email),
+    uniqueNameAndAge: uniqueIndex("uniqueNameAndAge").on(table.name, table.age),
+  }
 })
 
