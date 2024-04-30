6d25bdd000b50a02e3faef5dacce3a3a97a07454
Newton Bofulo <newtonombese1@gmail.com>
2024-04-30 10:35:15 +0300

    Successful basic seup of Drizzile

---
+++ src/main.ts
@@ -0,0 +1,14 @@
+import 'dotenv/config'
+import { db } from "./drizzle/db";
+import { UserTable } from "./drizzle/schema";
+
+async function main() {
+  await db.insert(UserTable).values({
+    name: "Newton"
+  })
+
+  const user = await db.query.UserTable.findFirst()
+  console.log(user)
+}
+
+main()
